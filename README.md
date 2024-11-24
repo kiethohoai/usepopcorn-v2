d96485bcda079e0717422eae2f2abc2196338254
kiethohoai <kietho.060193@gmail.com>
2024-11-24 14:38:40 +0700

    006 Loading Questions from a Fake API

--- README.md
+++ README.md
@@ -61,6 +61,6 @@
 ### 003 Managing Related Pieces of State
 ### 004 Managing State With useReducer
 ### 005 The React Quiz App
-
+### 006 Loading Questions from a Fake API
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import MainApp from './MainApp';
+import { useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+  status: 'loading', //loading, error, ready, active, finish
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: `ready`,
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: `error`,
+      };
+    default:
+      throw new Error(`Something went wrong!`);
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(() => dispatch({ type: 'dataFailed' }));
+  }, []);
   return (
     <div className="app">
       <Header />
